ccbcc8ccd31c2cd234b36a292e08521923c8b665
Matteo <matteo.gonzi99@gmail.com>
2024-12-31 11:19:50 +0100

    implemented logout logic and fix session storage header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,48 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, MenuItem, Menu } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 import { util } from '../utils/util';
 import { GiPirateSkull } from "react-icons/gi";
 import { CgProfile } from "react-icons/cg"
 import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 
 export default function Header() {
     const navigate = useNavigate();
     const [username, setUsername] = useState(undefined);
+    const [isAuth, setIsAuth] = useState(false);
+
+    const anchorEl = useRef(null);
+    const open = Boolean(anchorEl.current);
 
     useEffect(() => {
         const isUserAuthenticated = util.isAuthenticated();
-        if(isUserAuthenticated)
-            setUsername(sessionStorage.getItem('username'));
-      }, []);
+        const userLogged = sessionStorage.getItem('username'); 
+        if(isUserAuthenticated){
+            if(userLogged){
+                setIsAuth(true);
+                setUsername(sessionStorage.getItem('username'));
+            } else{
+                setIsAuth(true);
+                sessionStorage.setItem('username', 'pirata');
+                setUsername('pirata');
+                alert('Username missing in sessionStorage...Consider redirecting to login, set default pirata');
+            }
+        } else setIsAuth(false);
+    }, []);
+
+    const handleLogout = async () => {
+        await sessionStorage.setItem('username', undefined);
+        await Cookies.remove('authToken');
+        window.location.reload();
+    };
+
+    const handleMenuOpen = (event) => {
+        anchorEl.current = event.currentTarget;
+    };
+    
+    const handleMenuClose = () => {
+        anchorEl.current = null;
+    };
 
     return (
         <AppBar position="static">
@@ -22,10 +51,19 @@ export default function Header() {
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: 1 }} onClick={() => window.open('https://github.com/matteognz')}>
                     Treasure Hunt
                 </Typography>
-                { username ? (
+                { isAuth && username ? (
                     <>
                     <Typography variant="body1" marginRight={1}>Benvenuto, {username}!</Typography>
-                    <CgProfile size={32}/>
+                    <CgProfile size={32} aria-controls="logout-menu" aria-haspopup="true" onClick={handleMenuOpen} />
+                    <Menu
+                        id="logout-menu"
+                        anchorEl={anchorEl.current}
+                        open={open}
+                        onClose={handleMenuClose}
+                        MenuListProps={{ 'aria-labelledby': 'logout-menu' }}
+                    >
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
                     </>
                 ) : (
                     <div>
